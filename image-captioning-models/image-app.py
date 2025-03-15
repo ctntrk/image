97793bedc3638c548906bb138452530502fe0bad
Fatma Nur ÇETİNTÜRK <fatmanurctntrk@gmail.com>
2025-03-15 16:09:17 +0300

    Update image-app.py

--- image-captioning-models/image-app.py
+++ image-captioning-models/image-app.py
@@ -8,7 +8,7 @@ from transformers import BlipProcessor, BlipForConditionalGeneration
 import tempfile
 
 # Model yükleme fonksiyonu
-@st.experimental_singleton
+@st.singleton
 def load_model(model_type="base"):
     if model_type == "large":
         processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
